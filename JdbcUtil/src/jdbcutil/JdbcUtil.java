b36dd7287d6faaaa7632deb17929c82e076b94e5
Bruno Portela <31595407+brunoportela4@users.noreply.github.com>
2019-03-12 22:41:38 -0300

    "class not found", "no suitable driver"
    chegamos até "communication exception"

--- JdbcUtil/src/jdbcutil/JdbcUtil.java
+++ JdbcUtil/src/jdbcutil/JdbcUtil.java
@@ -1,17 +1,26 @@
 package jdbcutil;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class JdbcUtil {
 
     private static Connection conn;
-
+    private static String strConn="jdbc:mysql://127.0.0.1:3306/nomeDoBanco";
+    
     public static Connection getConnection() throws Exception {
-        Class.forName("org.gjt.mm.mysql.Driver");
+        //Class.forName("org.gjf.mm.mysql.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        if (JdbcUtil.conn != null){
             return JdbcUtil.conn;
+        }else {
+            JdbcUtil.conn = DriverManager.getConnection(strConn,"user","password");
+        }
+        return JdbcUtil.conn;
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println(JdbcUtil.getConnection());
     }
 }
+
